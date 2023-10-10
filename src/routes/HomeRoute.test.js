94a5869d0fb83887f4acfe6959a711c34dbf443e
Brandon Alexander <bjvonalexander@gmail.com>
2023-10-10 04:40:55 -0500

    Making a Reusable Create Server Function
    Test helper function server refactor

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,34 +1,26 @@
 import { render, screen } from "@testing-library/react";
-import { setupServer } from "msw/node";
-import { rest } from 'msw';
 import HomeRoute from "./HomeRoute";
-import {MemoryRouter} from "react-router";
+import { MemoryRouter } from "react-router";
+import { createServer } from "../test/server";
 
-const handlers = [
-    rest.get('api/repositories', (req, res, ctx) => {
-        const language = req.url.searchParams.get('q').split('language:')[1];
+// Goal:
 
-       return res(
-           ctx.json({
-               items: [
-                   { id: 1, full_name: `${language}_one`},
-                   { id: 2, full_name: `${language}_two`},
-               ],
-           })
-       );
-    }),
-];
-const server = setupServer(...handlers);
+createServer([
+    {
+        path: '/api/repositories',
+        method: 'get',
+        res: (req) => {
+            const language = req.url.searchParams.get('q').split('language:')[1];
+            return {
+                items: [
+                    { id: 1, full_name: `${language}_one`},
+                    { id: 2, full_name: `${language}_two`},
+                ]
+            }
+        }
+    }
+]);
 
-beforeAll(() => {
-    server.listen();
-});
-afterEach(() => {
-    server.resetHandlers();
-});
-afterAll(() => {
-    server.close();
-});
 
 test('renders two links for each language', async () => {
     render(
@@ -59,7 +51,3 @@ test('renders two links for each language', async () => {
         expect(links[1]).toHaveAttribute('href', `/repositories/${language}_two`);
     }
 });
-
-const pause = () => new Promise(resolve => {
-    setTimeout(resolve, 100);
-})
